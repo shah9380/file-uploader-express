ad5636609f48bf0025078d75f4e4700b49a0e410
shah9380 <shahislam9380@gmail.com>
2024-06-01 15:25:33 +0530

    cloudinary added for file upload

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express = require("express")
+const dotenv = require("dotenv").config()
+const bodyParser = require("body-parser")
+
+const cloudinary = require('cloudinary').v2
 
 const app = express();
+
+
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.use(bodyParser.json())
+
 
 app.listen(4000, ()=>{
     console.log('port listening at 4000');
